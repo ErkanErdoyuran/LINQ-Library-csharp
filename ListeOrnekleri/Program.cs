afe0903663e4adb89c56f558c29a2898c296b202
1erkan <erkn123-erdoyuran@outlook.com.tr>
2022-01-31 14:05:22 +0300

    Project files

---
+++ ListeOrnekleri/Program.cs
@@ -0,0 +1,14 @@
+﻿
+using System;
+using System.Linq;
+
+namespace ListeOrnekleri
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            new Uygulama().Calistir();
+        }
+    }
+}
